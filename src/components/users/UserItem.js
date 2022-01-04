bd722809cd251565241b83e4035be6f3a39267e9
ktresel2 <ktreseler2@gmail.com>
2022-01-03 22:03:33 -0500

    Start github finder project

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class UserItem extends Component {
+	constructor() {
+		super()
+		console.log(123)
+	}
+	render() {
+		return <div>UserItem</div>
+	}
+}
+
+export default UserItem

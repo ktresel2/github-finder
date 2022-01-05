8b3b47ee48ba76db790f3664561a15a62246f1db
ktresel2 <ktreseler2@gmail.com>
2022-01-04 21:28:11 -0500

    Change some components to functional

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,13 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
-export class UserItem extends Component {
-	constructor() {
-		super()
-		console.log(123)
-	}
-	render() {
-		return <div>UserItem</div>
-	}
+const UserItem = props => {
+	const { login, avatar_url, html_url } = props.user
+	return (
+		<div className="card text-center">
+			<img
+				className="round-img"
+				style={{ width: '60px' }}
+				src={avatar_url}
+				alt=""
+			/>
+			<h3>{login}</h3>
+			<div>
+				<a href={html_url} className="btn btn-dark btn-sm my-1">
+					Go to profile
+				</a>
+			</div>
+		</div>
+	)
+}
+
+UserItem.propTypes = {
+	user: PropTypes.object.isRequired,
 }
 
 export default UserItem
